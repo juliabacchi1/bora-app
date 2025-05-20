f3da87a4ed28514fed8564d9022b97818cb7fd34
Júlia Bacchi <134342345+juliabacchi1@users.noreply.github.com>
2025-05-20 14:02:20 -0300

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ListDetails from "./pages/ListDetails";
 
@@ -6,10 +6,12 @@ function App() {
   return (
     <div className="min-h-screen bg-[#e4e0df] flex justify-center">
       <div className="w-[390px] bg-[#e4e0df] min-h-screen shadow-xl">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/lista/:id" element={<ListDetails />} />
-        </Routes>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/lista/:id" element={<ListDetails />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </div>
   );
